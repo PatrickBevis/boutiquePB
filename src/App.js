f498d93f05155beb860ee8bdcbb89c2c0cf20849
PatrickBevis <bevis.patrick@gmail.com>
2022-09-06 14:01:35 +0200

    E9 fait

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
     <>
     <div className={'container-fluid ' + (isDark ?'bg-dark text-white': 'bg-white text-dark')}>
       <div class="form-check form-switch">
-  <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" onClick={handleOnClick} checked={isDark}/>
+  <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" onChange={handleOnClick} checked={isDark}/>
   <label class="form-check-label" for="flexSwitchCheckChecked">{isDark?'Dark':'light'}</label>
   </div>
       <HomeScreen />
